58893935c34985893a3fc7777404551444c687e2
Evin1- <edwin.hdz.12.0@gmail.com>
2016-05-04 22:22:11 -0400

    Add MixPanel tracking logic MainActivity

--- app/src/main/java/com/example/simplemixpanel/MainActivity.java
+++ app/src/main/java/com/example/simplemixpanel/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.simplemixpanel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String projectToken = getString(R.string.mixpanel_key);
+        MixpanelAPI mixpanel = MixpanelAPI.getInstance(this, projectToken);
+
+        try {
+            JSONObject props = new JSONObject();
+            props.put("Gender", "Female");
+            props.put("Logged in", false);
+            mixpanel.track("MainActivity - onCreate yes", props);
+        } catch (JSONException e) {
+            Log.e(TAG, "Unable to add properties to JSONObject", e);
+        }
     }
 }
